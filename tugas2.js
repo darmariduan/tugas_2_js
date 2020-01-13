ee3669f6ac6ef8d6049d415b56f5d96a6a915672
Darma Riduan <dharmarhiduwhan@gmail.com>
2020-01-13 08:50:15 +0700

    Tugas 2 js

---
+++ tugas2.js
@@ -0,0 +1,17 @@
+var number = 24;
+var string = 'Darma Riduan';
+var boolean = true;
+var cm = 170;
+var kosong ="ada";
+kosong = null;
+if(cm>160)
+tinggi = true;
+else
+tinggi = false;
+var array = [1,2,3,4,5];
+
+console.log("String", string);
+console.log("Number", number);
+console.log("boolean", tinggi);
+console.log("Null", kosong);
+console.log("Array", array[4]);
